637c241f70b6685ec94dfaba39eeb33848411076
Damien Leclair <damien.leclair@satimo.fr>
2014-09-02 09:17:14 +0200

    [Chapter11] Added Amin Controller

---
+++ Chapter 11/SportsStore/SportsStore.WebUI/Controllers/AdminController.cs
@@ -0,0 +1,20 @@
+﻿namespace SportsStore.WebUI.Controllers
+{
+    using System.Web.Mvc;
+    using SportsStore.Domain.Abstract;
+
+    public class AdminController : Controller
+    {
+        readonly IProductsRepository repository;
+
+        public AdminController(IProductsRepository repository)
+        {
+            this.repository = repository;
+        }
+        
+        public ViewResult Index()
+        {
+            return View(repository.Products);
+        }
+    }
+}
