ef5f92bb2bb2d6292e81cce257bd8f793489e43e
Damien Leclair <damien.leclair@satimo.fr>
2014-11-17 09:12:34 +0100

    [Chapter16] Retrieved code from previous chapter

---
+++ Chapter 16/UrlsAndRoutes/UrlsAndRoutes/App_Start/RouteConfig.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace UrlsAndRoutes
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.MapMvcAttributeRoutes();
+
+            routes.MapRoute(
+                "MyRoute",
+                "{controller}/{action}/{id}",
+                new { controller = "Home", action = "Index", id = UrlParameter.Optional });
+
+        }
+    }
+}
---
+++ Chapter 16/UrlsAndRoutes/UrlsAndRoutes/Controllers/CustomerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Web.Mvc;
+
+namespace UrlsAndRoutes.Controllers
+{
+    [RoutePrefix("User")]
+    public class CustomerController : Controller
+    {
+
+        [Route("~/Test")]
+        public ActionResult Index()
+        {
+            ViewBag.Controller = "Customer";
+            ViewBag.Action = "Index";
+            return View("ActionName");
+        }
+
+        [Route("Add/{user}/{id:int}")]
+        public string Create(string user, int id)
+        {
+            return string.Format("Create method - User : {0}, ID : {1}", user, id);
+        }
+
+        [Route("Add/{user}/{password:alpha:length(6)}")]
+        public string ChangePassword(string user, string password)
+        {
+            return string.Format("Change password method - User : {0}, Password : {1}", user, password);
+        }
+
+        public ActionResult List()
+        {
+            ViewBag.Controller = "Customer";
+            ViewBag.Action = "List";
+            return View("ActionName");
+        }
+	}
+}
