c2c980bd4c765fb1740e2ceffe929f4f51419ca0
Damien Leclair <damien.leclair@satimo.fr>
2015-03-30 14:02:16 +0200

    [FirstMvcApp] Added chapter 21 sample code

---
+++ Chapter 21/HelperMethods/HelperMethods/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Mvc;
+
+namespace HelperMethods.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewBag.Fruits = new[] { "Apple", "Orange", "Pear" };
+            ViewBag.Cities = new[] { "New York", "London", "Paris" };
+
+            const string message = "This is an HTML element : <input>";
+
+            return View((object)message);
+        }
+    }
+}
---
+++ Chapter 21/HelperMethods/HelperMethods/Infrastructure/CustomHelpers.cs
@@ -0,0 +1,26 @@
+﻿namespace HelperMethods.Infrastructure
+{
+    using System.Web.Mvc;
+
+    public static class CustomHelpers
+    {
+        public static MvcHtmlString ListArrayItems(this HtmlHelper html, string[] list)
+        {
+            var tag = new TagBuilder("ul");
+            foreach (var str in list)
+            {
+                var itemTag = new TagBuilder("li");
+                itemTag.SetInnerText(str);
+                tag.InnerHtml += itemTag.ToString();
+            }
+
+            return new MvcHtmlString(tag.ToString());
+        }
+
+        public static MvcHtmlString DisplayMessage(this HtmlHelper html, string message)
+        {
+            var result = string.Format("This is the message : <p>{0}</p>", message);
+            return new MvcHtmlString(result);
+        }
+    }
+}
