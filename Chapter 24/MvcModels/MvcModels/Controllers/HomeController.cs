d84c181322d2b8f97c81288e50e3785732c393f6
Damien Leclair <damien.leclair@satimo.fr>
2015-04-08 13:46:56 +0200

    [Chapter24] Added sample code

---
+++ Chapter 24/MvcModels/MvcModels/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace MvcModels.Controllers
+{
+    using System.Linq;
+    using MvcModels.Models;
+
+    public class HomeController : Controller
+    {
+        readonly IEnumerable<Person> personData = new[]
+        {
+            new Person { PersonId = 1, FirstName = "Adam", LastName = "Freeman", Role = Role.Admin },
+            new Person { PersonId = 2, FirstName = "Jacqui", LastName = "Griffyth", Role = Role.User },
+            new Person { PersonId = 3, FirstName = "John", LastName = "Smith", Role = Role.User },
+            new Person { PersonId = 4, FirstName = "Anne", LastName = "Jones", Role = Role.Guest }
+        };
+
+        // GET: Home
+        public ActionResult Index(int id)
+        {
+            return View(personData.First(p => p.PersonId == id));
+        }
+    }
+}
